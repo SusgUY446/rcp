120ca6ea5ec090993874e643acdbbca723d60ea3
SusgUY446 <susguy446@gmail.com>
2024-02-20 19:10:35 +0100

    moved command execution into index.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["seconds"])) { // if seconds are set the shutdown is executed
+        $seconds = $_POST["seconds"];
+        $command = "powershell.exe -Command \"shutdown /s /t $seconds\"";
+        exec($command);
+    } elseif (isset($_POST["message"]) && isset($_POST["title"])) { //if message and title are set the command for the message box is executed
+        $message = $_POST["message"];
+        $title = $_POST["title"];
+        $command = "powershell.exe -Command \"Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('$message', '$title')\"";
+        exec($command);
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,11 +20,11 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Remote Control Panel</h1>
+    <h1>RCP</h1>
 
     <div class="container">
         <h2>Shutdown</h2>
-        <form action="shutdown.php" method="post">
+        <form action="index.php" method="post">
             <label for="seconds">Shutdown in seconds</label><br>
             <input type="number" id="seconds" name="seconds"><br>
             <input type="submit" value="Submit">
@@ -18,7 +33,7 @@
 
     <div class="container">
         <h2>Message Box</h2>
-        <form action="msg_box.php" method="post">
+        <form action="index.php" method="post">
             <label for="title">Enter Title:</label><br>
             <input type="text" id="title" name="title"><br>
             <label for="message">Enter Message:</label><br>
--- shutdown.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $seconds = $_POST["seconds"];
-
-    $command = "powershell.exe -Command \"shutdown /s /t $seconds\"";
-    exec($command);
-}
-?>
